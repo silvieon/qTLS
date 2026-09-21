cfc7ad4d6cad01de99e802d94c02afebae6f948c
silvieon <ywang1051@gmail.com>
2026-09-21 18:29:17 -0400

    remove SFTP-specific elements

--- ftp_constants.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-# --- FTP CONFIGURATION ---
-FTP_PORT = 21
-SERVER_HOST = '127.0.0.1'
-BUFFER_SIZE = 4096 
-SERVER_ROOT = './secure_ftp_root'
-USER_CREDENTIALS = {"testuser": "password123"}
-TIMEOUT = 5
